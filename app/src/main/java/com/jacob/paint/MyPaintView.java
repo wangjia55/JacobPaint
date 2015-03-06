f2cb1008f01d422328858c78408067985d703a3c
王佳 <wangjia@cvte.cn>
2015-03-06 09:15:07 +0800

    完成面板绘制的功能

---
+++ app/src/main/java/com/jacob/paint/MyPaintView.java
@@ -0,0 +1,85 @@
+package com.jacob.paint;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.CornerPathEffect;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+
+/**
+ * Package : com.jacob.paint
+ * Author : jacob
+ * Date : 15-3-5
+ * Description : 这个类是用来xxx
+ */
+public class MyPaintView extends View {
+
+    private Paint mPaint;
+    private Path mPath;
+    private int mScreenWidth;
+    private int mScreenHeight;
+
+    public MyPaintView(Context context) {
+        this(context, null);
+    }
+
+    public MyPaintView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public MyPaintView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+        mScreenWidth = displayMetrics.widthPixels;
+        mScreenHeight = displayMetrics.heightPixels;
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setStrokeWidth(5);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setPathEffect(new CornerPathEffect(3));
+
+        mPath = new Path();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(mScreenWidth, mScreenHeight);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+//        super.onDraw(canvas);
+        canvas.drawPath(mPath, mPaint);
+    }
+
+    float x = 0;
+    float y = 0;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                x = event.getX();
+                y = event.getY();
+                mPath.lineTo(x, y);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                x = event.getX();
+                y = event.getY();
+                mPath.lineTo(x, y);
+                break;
+        }
+        invalidate();
+        return true;
+//        return super.onTouchEvent(event);
+    }
+}
